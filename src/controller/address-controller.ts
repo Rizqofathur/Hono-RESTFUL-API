7fe49dca89e2e0a26746b5a361b3181a3cd0b73f
unknown <fathur.risqo@gmail.com>
2025-01-09 12:11:32 +0700

    Add Get Address data in address controller

--- src/controller/address-controller.ts
+++ src/controller/address-controller.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 import { ApplicationVariables } from '../model/app-model';
 import { authMiddleware } from '../middleware/auth-middleware';
 import { User } from '@prisma/client';
-import { CreateAdressRequest } from '../model/address-model';
+import { CreateAdressRequest, GetAddressRequest } from '../model/address-model';
 import { AddressService } from '../service/address-service';
 export const addressController = new Hono<{ Variables: ApplicationVariables }>();
 
@@ -16,6 +16,21 @@ addressController.post('/api/contacts/:id/addresses', async (c) => {
   request.contact_id = contactId;
 
   const response = await AddressService.create(user, request);
+
+  return c.json({
+    data: response,
+  });
+});
+
+addressController.get('/api/contacts/:contact_id/adresses/:id', async (c) => {
+  const user = c.get('user') as User;
+  const contactId = Number(c.req.param('contact_id'));
+  const addressId = Number(c.req.param('id'));
+  const request: GetAddressRequest = {
+    contact_id: contactId,
+    id: addressId,
+  };
+  const response = await AddressService.get(user, request);
 
   return c.json({
     data: response,
