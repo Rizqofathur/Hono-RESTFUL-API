565411d75aba8c45d3ce4141225e5029944d0a15
unknown <fathur.risqo@gmail.com>
2025-01-10 22:46:00 +0700

    Add delete API method in addressController class

--- src/controller/address-controller.ts
+++ src/controller/address-controller.ts
@@ -2,50 +2,61 @@ import { Hono } from 'hono';
 import { ApplicationVariables } from '../model/app-model';
 import { authMiddleware } from '../middleware/auth-middleware';
 import { User } from '@prisma/client';
-import { CreateAdressRequest, GetAddressRequest, UpdateAddressRequest } from '../model/address-model';
+import { CreateAdressRequest, GetAddressRequest, RemoveAddressRequest, UpdateAddressRequest } from '../model/address-model';
 import { AddressService } from '../service/address-service';
 export const addressController = new Hono<{ Variables: ApplicationVariables }>();
 
 addressController.use(authMiddleware);
 
-addressController.post('/api/contacts/:id/addresses', async (c) => {
+addressController.post('/api/contacts/:contact_id/addresses', async (c) => {
   const user = c.get('user') as User;
-  const contactId = Number(c.req.param('id'));
+  const contactId = Number(c.req.param('contact_id'));
 
   const request = (await c.req.json()) as CreateAdressRequest;
   request.contact_id = contactId;
 
   const response = await AddressService.create(user, request);
-
   return c.json({
     data: response,
   });
 });
 
-addressController.get('/api/contacts/:contact_id/addresses/:id', async (c) => {
+addressController.get('/api/contacts/:contact_id/addresses/:address_id', async (c) => {
   const user = c.get('user') as User;
   const request: GetAddressRequest = {
     contact_id: Number(c.req.param('contact_id')),
-    id: Number(c.req.param('id')),
+    id: Number(c.req.param('address_id')),
   };
-  const response = await AddressService.get(user, request);
 
+  const response = await AddressService.get(user, request);
   return c.json({
     data: response,
   });
 });
 
-addressController.put('/api/contacts/:contact_id/addresses/:id', async (c) => {
+addressController.put('/api/contacts/:contact_id/addresses/:address_id', async (c) => {
   const user = c.get('user') as User;
   const contactId = Number(c.req.param('contact_id'));
-  const addressId = Number(c.req.param('id'));
+  const addressId = Number(c.req.param('address_id'));
   const request = (await c.req.json()) as UpdateAddressRequest;
 
   request.contact_id = contactId;
   request.id = addressId;
 
   const response = await AddressService.update(user, request);
+  return c.json({
+    data: response,
+  });
+});
 
+addressController.delete('/api/contacts/:contact_id/addresses/:address_id', async (c) => {
+  const user = c.get('user') as User;
+  const request: RemoveAddressRequest = {
+    contact_id: Number(c.req.param('contact_id')),
+    id: Number(c.req.param('address_id')),
+  };
+
+  const response = await AddressService.remove(user, request);
   return c.json({
     data: response,
   });
